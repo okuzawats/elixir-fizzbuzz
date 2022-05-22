b633875c532396566ddd85585e1349c226ed7224
okuzawats <okuzawats@gmail.com>
2022-05-22 22:31:21 +0900

    write module attributes

--- fizzbuzz.ex
+++ fizzbuzz.ex
@@ -1,4 +1,14 @@
 defmodule FizzBuzz do
+  @moduledoc """
+  Module to run FizzBuzz.
+  """
+
+  @doc """
+  returns "FizzBuzz" if `n` is divisible by both three and five,
+  returns "Fizz" if `n` is divisible by three,
+  returns "Buzz" if `n` is divisible by five,
+  else returns `n`.
+  """
   def fb(n) when rem(n, 15) == 0, do: "FizzBuzz"
   def fb(n) when rem(n, 5) == 0, do: "Fizz"
   def fb(n) when rem(n, 3) == 0, do: "Buzz"
