a7213ed419cd75703dadc2d8fef8ac1f4aad20ba
okuzawats <okuzawats@gmail.com>
2022-05-25 21:54:27 +0900

    add test

--- fizzbuzz.ex
+++ fizzbuzz.ex
@@ -14,3 +14,18 @@ defmodule FizzBuzz do
   def fb(n) when rem(n, 5) == 0, do: "Buzz"
   def fb(n), do: n
 end
+
+ExUnit.start()
+
+defmodule FizzBuzzTest do
+  use ExUnit.Case
+
+  test "fb(1) returns 1", do: assert FizzBuzz.fb(1) == 1
+  test "fb(2) returns 2", do: assert FizzBuzz.fb(2) == 2
+  test "fb(3) returns Fizz", do: assert FizzBuzz.fb(3) == "Fizz"
+  test "fb(4) returns 4", do: assert FizzBuzz.fb(4) == 4
+  test "fb(5) returns Buzz", do: assert FizzBuzz.fb(5) == "Buzz"
+  test "fb(6) returns Fizz", do: assert FizzBuzz.fb(6) == "Fizz"
+  test "fb(7) returns 7", do: assert FizzBuzz.fb(7) == 7
+  test "fb(15) returns FizzBuzz", do: assert FizzBuzz.fb(15) == "FizzBuzz"
+end
