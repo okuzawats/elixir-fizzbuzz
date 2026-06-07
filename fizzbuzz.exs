ba6dc77315a903e3da46d32a6d6104f53e5d8b72
okuzawats <okuzawats@gmail.com>
2026-06-07 14:56:37 +0900

    define fizzbuzz function

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -5,4 +5,10 @@ defmodule FizzBuzz do
   defguardp fizz(n) when rem(n, 3) == 0
   # `buzz/1` true if `n` is divisible by 5
   defguardp buzz(n) when rem(n, 5) == 0
+
+  @doc "returns FizzBuzz, Fizz, Buzz or the number in string."
+  def run(n) when fizz(n) and buzz(n), do: "FizzBuzz"
+  def run(n) when fizz(n), do: "Fizz"
+  def run(n) when buzz(n), do: "Buzz"
+  def run(n), do: "#{n}"
 end
