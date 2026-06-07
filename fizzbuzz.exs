27e81bfcaa337173d3d06f7811d661f5d20444f4
okuzawats <okuzawats@gmail.com>
2026-06-07 13:29:07 +0900

    define some defguardp

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -1,3 +1,8 @@
 defmodule FizzBuzz do
   @moduledoc "run FizzBuzz."
+
+  # `fizz/1` true if `n` is divisible by 3
+  defguardp fizz(n) when rem(n, 3) == 0
+  # `buzz/1` true if `n` is divisible by 5
+  defguardp buzz(n) when rem(n, 5) == 0
 end
